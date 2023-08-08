2455a39a4e1f86fdff470fa693dd5a72be5776a9
Александр Кубышин <gigobug@gmail.com>
2023-08-09 01:49:57 +0300

    filter for index

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,20 @@
+# Для применения Q-объектов их нужно импортировать:
+from django.db.models import Q
 from django.shortcuts import render
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        Q(is_published=True) & (
+            Q(is_on_main=True) | Q(title__contains='пломбир')
+        )
+    )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
