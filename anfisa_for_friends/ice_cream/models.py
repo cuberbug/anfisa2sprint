90c8f3bbe722f2595cf7524dc498e55d098e546f
Александр Кубышин <gigobug@gmail.com>
2023-08-10 01:51:39 +0300

    upgrade

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -86,10 +86,22 @@ class IceCream(PublishedModel):
         default=False,
         verbose_name='На главную',
     )
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
 
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+        # Сначала сортируем по полю output_order,
+        # а если у нескольких объектов значения output_order совпадают--
+        # сортируем по title.
+        ordering = ('output_order', 'title')
 
     def __str__(self) -> str:
         return self.title
